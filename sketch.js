0fb9dd54235ad41795115074f863a369e930626b
letorvar <letorvar@gmail.com>
2017-05-09 23:17:00 +0200

    refactoring to game objects

--- sketch.js
+++ sketch.js
@@ -13,9 +13,34 @@ var bRange = 255;
 var numberOfColors = 3;
 var availableColors;
 
+var rects = [];
+function RandomColor(){
+    var index = floor(random(numberOfColors));
+    var randColor = availableColors[index];
+    this.r = randColor.r;
+    this.g = randColor.g;
+    this.b = randColor.b;
+}
+function ColorfulRect(x,y,color){
+    this.x = x;
+    this.y = y;
+    this.color = color; 
+
+
+    this.draw = function(){
+        fill(this.color.r, this.color.g, this.color.b);
+        rect(this.x * rectSize, this.y * rectSize, rectSize, rectSize);    
+    }
+}
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
     availableColors = drawColors(numberOfColors);
+    for (var x = 0; x < amountOfRectsInRow; x++) {
+        for (var y = 0; y < amountOfRectsInRow; y++) {
+            rects.push(new ColorfulRect(x,y,new RandomColor()));
+        }
+    }
+    
     // availableColors = [
     //     { r: 230, g: 5, b: 2 },
     //     { r: 8, g: 240, b: 6 },
@@ -25,24 +50,27 @@ function setup() {
 }
 
 function draw() {
-    var index = 0;
-    var randomCoordinates = drawCoordinate();
-    for (var x = 0; x < amountOfRectsInRow; x++) {
-        for (var y = 0; y < amountOfRectsInRow; y++) {
-            // randColor = drawColor(rRange, gRange, bRange);
-            index = floor(random(numberOfColors)); // losuje jeden z indeksów 
-            randColor = availableColors[index]; //losuje jeden z kolorów z tabilcy 
-
-            fill(randColor.r, randColor.g, randColor.b);
-            rect(x * rectSize, y * rectSize, rectSize, rectSize);
-            if (randomCoordinates.x === x && randomCoordinates.y === y) {
-                index++;
-                randColor = availableColors[index % numberOfColors];
-                fill(randColor.r, randColor.g, randColor.b);
-            }
-            rect(x * rectSize + boardSize + margin, y * rectSize, rectSize, rectSize);
-        }
+    for (var i = 0; i < rects.length; i++){
+        rects[i].draw();
     }
+    // var index = 0;
+    // var randomCoordinates = drawCoordinate();
+    // for (var x = 0; x < amountOfRectsInRow; x++) {
+    //     for (var y = 0; y < amountOfRectsInRow; y++) {
+    //         // randColor = drawColor(rRange, gRange, bRange);
+    //         index = floor(random(numberOfColors)); // losuje jeden z indeksów 
+    //         randColor = availableColors[index]; //losuje jeden z kolorów z tabilcy 
+
+    //         fill(randColor.r, randColor.g, randColor.b);
+    //         rect(x * rectSize, y * rectSize, rectSize, rectSize);
+    //         if (randomCoordinates.x === x && randomCoordinates.y === y) {
+    //             index++;
+    //             randColor = availableColors[index % numberOfColors];
+    //             fill(randColor.r, randColor.g, randColor.b);
+    //         }
+    //         rect(x * rectSize + boardSize + margin, y * rectSize, rectSize, rectSize);
+    //     }
+    // }
 }
 
 function drawCoordinate() {
