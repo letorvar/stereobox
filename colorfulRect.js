933d878dcb2ae78d2bdbc8803a10efc8386c43c6
letorvar <letorvar@gmail.com>
2017-06-03 23:59:23 +0200

    refactoring of 'if statement'

--- colorfulRect.js
+++ colorfulRect.js
@@ -2,34 +2,31 @@ function ColorfulRect(x, y, color) {
     this.x = x;
     this.y = y;
     this.color = color;
-
-    this.drawYourself = function (offsetX, offsetY) {
-        if (mouseX >= this.x * rectSize && mouseX < this.x * rectSize + rectSize &&
-            mouseY >= this.y * rectSize && mouseY <this.y * rectSize + rectSize
-        ){ 
-           fill(this.color.r, this.color.g, this.color.b);
-           strokeWeight(10);
-           stroke(0);
-        } else{
+    this.isCursorInsideRect = function() {
+        return (mouseX >= this.x * rectSize && mouseX < this.x * rectSize + rectSize &&
+            mouseY >= this.y * rectSize && mouseY < this.y * rectSize + rectSize);
+    }
+    this.drawYourself = function(offsetX, offsetY) {
+        if (this.isCursorInsideRect()) {
+            fill(this.color.r, this.color.g, this.color.b);
+            strokeWeight(10);
+            stroke(0);
+        } else {
             fill(this.color.r, this.color.g, this.color.b);
             strokeWeight(1);
         }
         rect(this.x * rectSize + offsetX, this.y * rectSize + offsetY, rectSize, rectSize);
     }
-    this.clicked = function (mouseX, mouseY) {
+    this.clicked = function(mouseX, mouseY) {
         // console.log(this.x + " " + this.y);
-        if (mouseX >= this.x * rectSize && mouseX < this.x * rectSize + rectSize &&
-            mouseY >= this.y * rectSize && mouseY <this.y * rectSize + rectSize
-        ){ 
-            this.color = new RandomColor(); 
+        if (this.isCursorInsideRect()) {
+            this.color = new RandomColor();
         }
     }
-    this.mouseOver = function (mouseX, mouseY) {
-        if (mouseX >= this.x * rectSize && mouseX < this.x * rectSize + rectSize &&
-            mouseY >= this.y * rectSize && mouseY <this.y * rectSize + rectSize
-        ){     
-        fill(this.color.r, this.color.g, this.color.b, 5);
-        rect(this.x * rectSize, this.y * rectSize, rectSize, rectSize);
+    this.mouseOver = function(mouseX, mouseY) {
+        if (this.isCursorInsideRect()) {
+            fill(this.color.r, this.color.g, this.color.b, 5);
+            rect(this.x * rectSize, this.y * rectSize, rectSize, rectSize);
         }
     }
 }
